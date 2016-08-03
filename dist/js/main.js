10b3ceffcd9f2ae7acfc757d2780f11b0256b756
Wagner Souza <wagner.junior@fh.com.br>
2016-08-03 16:19:20 -0300

    syncing

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,48 +15,76 @@ var RequestBlockerApp = (function( $, angular, chrome, window, document, undefin
 
     RequestBlocker.fn = RequestBlocker.prototype = {
         init: function() {
-            console.warn('ini');
+            RequestBlocker.angular().loadLocalData(function( data ) {
+                if( chrome.webRequest.onBeforeRequest.hasListener( blockRequest ) ) {
+                    chrome.webRequest.onBeforeRequest.removeListener( blockRequest );
+                }
+
+if (patterns.length) {
+        chrome.webRequest.onBeforeRequest.addListener(blockRequest, {
+            urls: patterns
+        }, ['blocking']);
+        console.info('Using patterns:');
+        for (p in patterns) {
+            console.info(patterns[p]);
+        }
+    } else {
+        console.warn('No patterns defined.');
+    }
+            });
         }
     };
 
-    app.controller('PopupController', function( $scope ) {
-        $scope.brand = 'HTTP Request Blocker';
+    RequestBlocker.fn.loadLocalData = function( callback ) {
+        chrome.storage.sync.get('blocked_patterns', function( data ) {
+            callback( data['blocked_patterns'] || [] );
+        });
+    };
 
-        $scope.edit = function( id ) {
-            var $link = $('[data-link-id="' + id + '"]'),
-                $modal = $('#edit-link-modal');
+    RequestBlocker.fn.helpers = {
+        parseLink: function( url ) {
+            var _link = document.createElement('a');
+                _link.href = url;
 
-            var title = $link.find('.link-url').text(),
-                hostname = parseLink( title ).hostname;
-
-            $modal.find('.modal-header .modal-title').text( hostname );
-            $modal.modal();
-        };
-    })
-    .directive('modals', function() {
-        return {
-            templateUrl: 'dist/js/templates/modals.html',
-            replace: true
-        };
-    })
-    .directive('navbar', function() {
-        return {
-            templateUrl: 'dist/js/templates/navbar.html',
-            replace: true
-        };
-    })
-    .directive('testing', function() {
-        return {
-            templateUrl: 'dist/js/templates/table.html',
-            replace: true
+            return _link;
         }
-    });
+    };
 
-    function parseLink( url ) {
-        var _link = document.createElement('a');
-            _link.href = url;
+    RequestBlocker.fn.angular = function() {
+        app.controller('PopupController', function( $scope ) {
+            $scope.brand = 'HTTP Request Blocker';
 
-        return _link;
+            $scope.edit = function( id ) {
+                var $link = $('[data-link-id="' + id + '"]'),
+                    $modal = $('#edit-link-modal');
+
+                var title = $link.find('.link-url').text(),
+                    hostname = parseLink( title ).hostname;
+
+                $modal.find('.modal-header .modal-title').text( hostname );
+                $modal.modal();
+            };
+        })
+        .directive('modals', function() {
+            return {
+                templateUrl: 'dist/js/templates/modals.html',
+                replace: true
+            };
+        })
+        .directive('navbar', function() {
+            return {
+                templateUrl: 'dist/js/templates/navbar.html',
+                replace: true
+            };
+        })
+        .directive('testing', function() {
+            return {
+                templateUrl: 'dist/js/templates/table.html',
+                replace: true
+            }
+        });
+
+        return this;
     };
 
     window.requestBlocker = requestBlocker = RequestBlocker();
